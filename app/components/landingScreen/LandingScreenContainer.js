123de75c6ebfc76040e93731fed3c34fa5850a04
Akhilesh Mourya <akhilmorya1206@gmail.com>
2020-12-07 20:51:52 +0530

    Hand;ed navigation

--- app/components/landingScreen/LandingScreenContainer.js
+++ app/components/landingScreen/LandingScreenContainer.js
@@ -9,6 +9,12 @@ class LandingScreenContainer extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+    if(this.props.user) {
+      this.props.navigation.replace('HomeScreen'); // If already logged in
+    }
+  }
+
   componentDidUpdate(prevProp) {
     if (this.props.user && this.props.user !== prevProp.user) {
       NavigationService.navigateAndReset('HomeScreen');
